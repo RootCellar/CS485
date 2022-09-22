06b8458d58e662f6bcd353bd505ccf2dbf7f7b75
RootCellar <RootCellar9877@GMail.com>
2022-09-22 12:43:11 -0800

    mostly finish HW1

---
+++ HW1/UpperArmAttach.gd
@@ -0,0 +1,22 @@
+extends MeshInstance
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if(Input.is_physical_key_pressed(KEY_W)):
+		transform.basis.y += 0.02*transform.basis.z
+	if(Input.is_physical_key_pressed(KEY_S)):
+		transform.basis.y += -0.02*transform.basis.z
+		
+	transform.basis = transform.basis.orthonormalized()
+	pass
